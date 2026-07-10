370e95955fe8e8ce4764fc160e4d3235758049c4
TanishaSaini28 <sainitanisha091@gmail.com>
2026-07-10 19:15:05 +0530

    Create six.java

---
+++ six.java
@@ -0,0 +1,10 @@
+public class six {
+    static void change(int a){
+        a = 100;
+    }
+    public static void main(String[] args) {
+        int a = 10 ; 
+        change(a);
+        System.out.println(a); 
+    }
+}
