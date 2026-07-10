b1a99313a581ac2bb92df32d720a9bd6a65660ee
TanishaSaini28 <sainitanisha091@gmail.com>
2026-07-10 19:13:48 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,9 @@
+public class Even {
+    static boolean isEven(int n){
+        return n%2==0;
+    }
+    public static void main(String[] args){
+        System.out.println(isEven(20));
+        System.out.println(isEven(15));
+    }
+}
