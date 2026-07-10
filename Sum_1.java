48e33275ed166d933b6319adafca735e4f262cd7
TanishaSaini28 <sainitanisha091@gmail.com>
2026-07-10 18:25:04 +0530

    Create Sum_1.java

---
+++ Sum_1.java
@@ -0,0 +1,10 @@
+ public class Sum_1{
+
+    static int add (int a , int b){
+        int c = a+b;
+        return c ; 
+    }
+    public static void main(String[] args){
+        System.out.print(add(30,40));
+    }
+}
