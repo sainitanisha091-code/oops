7f1d46de518e3615bebc3d1d5cfcb14f87480b44
TanishaSaini28 <sainitanisha091@gmail.com>
2026-07-10 19:17:20 +0530

    Create type.java

---
+++ type.java
@@ -0,0 +1,12 @@
+public class type {
+    static void type(int a){
+        System.out.println("Integer");
+    }
+    static void type(double a){
+        System.out.println("double");
+    }
+    public static void main(String[] args) {
+        type(20);
+        type(20.5);
+    }
+} 
