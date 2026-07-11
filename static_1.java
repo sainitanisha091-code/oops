8132359a12a0d02cba5d251f6b396776914cbe2a
TanishaSaini28 <sainitanisha091@gmail.com>
2026-07-11 12:19:16 +0530

    Create static_1.java

---
+++ static_1.java
@@ -0,0 +1,12 @@
+public class static_1 {
+    static {
+        System.out.println("Static Block");
+    }
+    static {
+        System.out.println("Static123 block");
+    }
+    public static void main(String[] args) {
+        System.out.println("Main Block");
+    }
+}
+ 
