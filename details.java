4f89f85bad0ff82f0bf1905d16bd3ed6772d65fa
TanishaSaini28 <sainitanisha091@gmail.com>
2026-07-11 12:10:01 +0530

    Create details.java

---
+++ details.java
@@ -0,0 +1,11 @@
+public class details {
+    static void details(String n , int a ){
+        System.out.println("My Name is "+ n +" my age is "+a);    
+    }
+    public static void main(String[] args) {
+        String name = "Tanisha";
+        int age = 24; 
+        details(name, age);
+    }
+    
+} 
