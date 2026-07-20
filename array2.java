059aad084411d8bb08da4b4fcfbde56b3668cb95
TanishaSaini28 <sainitanisha091@gmail.com>
2026-07-20 09:48:53 +0530

    Create array2.java

---
+++ array2.java
@@ -0,0 +1,18 @@
+
+public class array2 {
+    public static void main(String[] args) {
+
+        int arr[][] = {
+                { 10, 20, 30 },
+                { 40, 50, 60 },
+                { 70, 80, 90 }
+        };
+        for (int i = 0 ; i < arr.length ;i++){
+            for (int j = 0 ; j < arr.length ;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+}
