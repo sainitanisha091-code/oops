2415a9303cc3df9c60689b3598f44080a2433390
TanishaSaini28 <sainitanisha091@gmail.com>
2026-07-21 20:58:05 +0530

    Create Foundation.java

---
+++ Foundation.java
@@ -0,0 +1,12 @@
+class Stud{
+    String name;
+    int age;
+    public static void main(String[] args) {
+        Stud s1 = new Stud();
+        s1.name = "Tanisha";
+        s1.age = 19;
+
+        System.out.println(s1.name);
+        System.out.println(s1.age);
+    }
+}
