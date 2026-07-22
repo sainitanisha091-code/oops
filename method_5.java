8db1e4556ec9a62873373dff66b4b71dc057b0ef
TanishaSaini28 <sainitanisha091@gmail.com>
2026-07-22 09:49:01 +0530

    Create method_5.java

---
+++ method_5.java
@@ -0,0 +1,13 @@
+public class method_5 {
+    static void multi(int n){
+        for (int i = 1 ; i <= 10 ; i++){
+           System.out.println(n*i);  
+        }
+    }
+    public static void main(String[] args) {
+        method_5 obj = new method_5();
+         obj.multi(5);
+       
+    }
+
+}
