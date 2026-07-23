654d79230e3a56940d9436124b2eb3562b01e0ab
TanishaSaini28 <sainitanisha091@gmail.com>
2026-07-23 21:11:20 +0530

    Create upper.java
    convert string to uppercase

---
+++ upper.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class upper {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        String str = sc.nextLine();
+        String result = str.toUpperCase();
+        System.out.print(result);
+        sc.close();
+    }
+}
