c094ce8ab051161d84d5bbb038e6f607e4c9fc8b
TanishaSaini28 <sainitanisha091@gmail.com>
2026-07-23 20:19:28 +0530

    Create copy_array.java

---
+++ copy_array.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class copy_array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] arr1 = new int[n];
+        int [] arr2 = new int[n];
+        for (int i = 0  ; i < n ;i++){
+            arr1[i] = sc.nextInt();
+        }
+        for (int i = 0  ; i < n ;i++){
+            arr2[i] = arr1[i];
+        }
+        for (int i = 0  ; i < n ;i++){
+            System.out.print(arr2[i]+" ");
+        }
+        sc.close();
+
+    }
+}
