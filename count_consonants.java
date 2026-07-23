96462641b4afaac70fd5dbeab233bdb424784a5b
TanishaSaini28 <sainitanisha091@gmail.com>
2026-07-23 21:52:28 +0530

    Create count_consonants.java

---
+++ count_consonants.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class count_consonants{
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        String str = sc.nextLine();
+        int count = 0 ;
+        for (int i = 0 ; i < str.length() ; i++){
+            char ch = str.charAt(i);
+            if (ch!='a'&& ch !='e'&&ch!='i'&& ch !='o' && ch!= 'u' ){
+                count++;
+            }
+        }
+        System.out.println(count);
+        sc.close();
+
+    }
+}
