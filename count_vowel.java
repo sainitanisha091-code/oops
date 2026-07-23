4dc7450d51c5b0a90f13012f1d1cec7e6259eff0
TanishaSaini28 <sainitanisha091@gmail.com>
2026-07-23 21:44:52 +0530

    Create count_vowel.java

---
+++ count_vowel.java
@@ -0,0 +1,17 @@
+import java.util.*;;
+public class count_vowel {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int count = 0 ; 
+        str = str.toLowerCase();
+        for (int i = 0 ;i < str.length(); i++){
+            char ch = str.charAt(i);
+            if (ch=='a'|| ch =='e'||ch=='i'|| ch =='o'|| ch=='u'){
+                count++;
+            }
+        }
+        System.out.println(count);
+        sc.close();
+    }
+}
