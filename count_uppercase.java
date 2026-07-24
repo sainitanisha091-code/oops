72a43132b487f1b55da9469959b54ff8cd157bef
TanishaSaini28 <sainitanisha091@gmail.com>
2026-07-24 11:32:17 +0530

    Create count_uppercase.java

---
+++ count_uppercase.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class count_uppercase {
+    public static void main(String[] args) {
+    Scanner sc  = new Scanner(System.in);
+    String str = sc.nextLine();
+    int count = 0 ;
+    for (int i = 0 ; i < str.length(); i++){
+        char ch = str.charAt(i);
+        if (ch>='A' && ch<='Z'){
+            count++;
+        }
+    }
+    System.out.print(count);
+    sc.close();
+    
+ }
+}
