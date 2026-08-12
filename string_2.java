c087f4eef8b0cfe01a17a447451a423207ecafb2
TanishaSaini28 <sainitanisha091@gmail.com>
2026-08-12 05:38:07 +0530

    Create string_2.java

---
+++ string_2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class string_2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
