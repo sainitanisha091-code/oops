11b0631ed708db7117691b76eebb5ed55e0022f3
TanishaSaini28 <sainitanisha091@gmail.com>
2026-08-13 21:22:14 +0530

    Create array_12.java

---
+++ array_12.java
@@ -0,0 +1,15 @@
+import java.util.*; 
+public class array_12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[10];
+        try {
+            for (int i = 0; i < 10; i++) {
+                System.out.print(arr[11]);
+            }
+        } catch (Exception e) {
+            System.out.println("exception");
+        }
+        sc.close();
+    }
+}
