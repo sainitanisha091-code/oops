f81bf8dc525a92c2d636c18453ba7d868696739e
TanishaSaini28 <sainitanisha091@gmail.com>
2026-08-18 10:31:51 +0530

    Create wrapper2.java

---
+++ wrapper2.java
@@ -0,0 +1,9 @@
+public class wrapper2 {
+    public static void main(String[] args) {
+        int a = 50 ; 
+        Integer b = a ; 
+        int c = b ;
+        System.out.println(b);
+        System.out.print(c);
+    }
+}
