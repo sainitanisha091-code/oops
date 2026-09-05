b26d7347ab675eb4b6dbcf2220e65118f24bc810
Tanisha <sainitanisha091@gmail.com>
2026-09-05 17:56:48 +0530

    Create binarySubstring.java
    Given a binary string s. You have to count the number of substrings that start and end with 1.
    
    Examples:
    
    Input: s = "1111"
    Output: 6
    Explanation: There are 6 substrings from the given string. They are "11", "11", "11", "111", "111", "1111".
    Input: s = "01101"
    Output: 3
    Explanation: There are 3 substrings from the given string. They are "11", "101", "1101".

---
+++ binarySubstring.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int binarySubstring(String s) {
+        int count = 0 ;
+        int ans = 0 ;
+        for (int i = 0 ; i< s.length(); i++){
+            if(s.charAt(i)=='1'){
+                count++;
+            }
+         
+       
+    }
+     return count * (count - 1) / 2;
+}
+}
