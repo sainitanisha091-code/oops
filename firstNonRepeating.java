1e4541a965115efd4178c529534d7cfa54df2974
Tanisha <sainitanisha091@gmail.com>
2026-09-10 09:34:12 +0530

    Create firstNonRepeating.java
    A text-processing system receives a string and needs to identify the first character that appears only once in the string.
    
    If every character occurs more than once, print -1.

---
+++ firstNonRepeating.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public static String firstNonRepeating(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            int count = 0;
+
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    count++;
+                }
+            }
+
+            if (count == 1) {
+                return String.valueOf(s.charAt(i));
+            }
+        }
+
+        return "-1";
+    }
+}
